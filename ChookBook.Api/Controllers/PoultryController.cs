3b7ba06791348547e06a2b3c6092cd19e4b7608a
Richard Harper <dick.harper@gmail.com>
2017-07-13 17:57:32 -0600

    Work on poultry controller

--- ChookBook.Api/Controllers/PoultryController.cs
+++ ChookBook.Api/Controllers/PoultryController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using ChookBook.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,25 +10,46 @@ namespace ChookBook.Api.Controllers
     [Route("/api/poultry")]
     public class PoultryController : Controller
     {
+        private readonly IEnumerable<BirdDto> poultry = new List<BirdDto>
+        {
+            new BirdDto
+            {
+                Id = new Guid("F8E0CAFEA4214645957BBE477C0B821E"),
+                Age = "14 months",
+                Breed = "Spotted Sussex",
+                Gender = "Female/Hen",
+                ImageFileName = "Images/Birds/F8E0CAFEA4214645957BBE477C0B821E.jpg",
+                NickName = "Lady Gray",
+                Type = "Chicken",
+                Weight = "3 lbs 6oz"
+            },
+            new BirdDto
+            {
+                Id = new Guid("78F83C04DADE4D8C9AD41AC8B3184017"),
+                Age = "14 months",
+                Breed = "Cornish",
+                Gender = "Male/Rooster",
+                ImageFileName = "Images/Birds/78F83C04DADE4D8C9AD41AC8B3184017.jpg",
+                NickName = "Pumpkin",
+                Type = "Chicken",
+                Weight = "4 lbs 8oz"
+            }
+        };
+
         [HttpGet()]
         public IActionResult GetPoultry()
         {
-            var birds = new List<BirdDto>
+            return Ok(poultry);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBird(Guid id)
+        {
+            if (poultry.Any(x => x.Id == id))
             {
-                new BirdDto
+                return Ok(new BirdDto
                 {
-                    Id = "F8E0CAFEA4214645957BBE477C0B821E",
-                    Age = "14 months",
-                    Breed = "Spotted Sussex",
-                    Gender = "Female/Hen",
-                    ImageFileName = "Images/Birds/F8E0CAFEA4214645957BBE477C0B821E.jpg",
-                    NickName = "Lady Gray",
-                    Type = "Chicken",
-                    Weight = "3 lbs 6oz",
-                },
-                new BirdDto
-                {
-                    Id = "78F83C04DADE4D8C9AD41AC8B3184017",
+                    Id = new Guid("78F83C04DADE4D8C9AD41AC8B3184017"),
                     Age = "14 months",
                     Breed = "Cornish",
                     Gender = "Male/Rooster",
@@ -35,9 +58,11 @@ namespace ChookBook.Api.Controllers
                     Type = "Chicken",
                     Weight = "4 lbs 8oz",
 
-                }
-            };
-            return Ok(birds);
+                });
+
+            }
+            return NotFound();
+            
         }
     }
 }
--- ChookBook.Api/Models/BirdDto.cs
+++ ChookBook.Api/Models/BirdDto.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ChookBook.Api.Models
 {
     public class BirdDto
     {
-        public string Id { get; set; }
+        public Guid Id { get; set; }
         public string Age { get; set; }
         public string Breed { get; set; }
         public string Gender { get; set; }
